447a8717d9c619617712a90fa585f2c15deb06ad
Will Osborne <willfosborne@gmail.com>
2018-11-11 13:23:57 +0000

    Huge nasty move to nix

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ reflex-platform ? import ./reflex-platform {} }:
+
+reflex-platform.project ({ pkgs, ... }: {
+  packages = {
+    common = ./common;
+    backend = ./backend;
+    frontend = ./frontend;
+  };
+
+  shells = {
+    ghc = ["common" "backend" "frontend"];
+    ghcjs = ["common" "frontend"];
+  };
+})
