c09f597cb843fc12673343a45463d0690983dc38
Will Osborne <willfosborne@gmail.com>
2018-11-12 17:13:52 +0000

    Rendering work
    Fills, colour, custom stroke

--- frontend/src/Content.hs
+++ frontend/src/Content.hs
@@ -8,16 +8,20 @@ type Font = String -- just represents the javascript name of the font
 data Content =
   Empty
   | Rect Double Double -- width, height TODO consider filled shapes
+  | FRect Double Double -- width, height TODO consider filled shapes
   | Circle Double -- radius
+  | FCircle Double -- radius
   | Line Double Double Double Double -- begin, end
   | Path [(Double, Double)] -- list of points to connect with path
   | Polygon [(Double, Double)] -- list of points to connect; join up at end
+  | FPolygon [(Double, Double)] -- list of points to connect; join up at end
   | RegularPolygon Int Double -- num. sides, radius
+  | FRegularPolygon Int Double -- num. sides, radius
   | Translate Double Double Content --  x, y, content to transform
   | Rotate Double Content -- angle, content 
   | Scale Double Double Content -- xscale, yscale, content
   | Combine Content Content -- combine two Contents   NOTE: consider renaming
   | Text Font (Maybe Float) String  -- font, max width, text  TODO: wrap text rather than squashing it!
-  | FillColor String -- fill color
-  | StrokeColor String -- fill color
+  | FillColor Int Int Int Float Content -- fill color
+  | StrokeColor Int Int Int Float Content -- fill color
   -- TODO add splines
--- frontend/src/Render.hs
+++ frontend/src/Render.hs
@@ -4,6 +4,12 @@ import Content
 
 import GHCJS.DOM.CanvasRenderingContext2D
 import GHCJS.DOM.CanvasPath
+import GHCJS.DOM.Enums (CanvasWindingRule (CanvasWindingRuleNonzero))
+import GHCJS.DOM.Types (CanvasStyle (..))
+
+import Data.List (intercalate)
+import GHCJS.Prim (toJSString)
+
 import qualified GHCJS.DOM.TextMetrics as TextMetrics (getWidth) 
 
 
@@ -17,9 +23,18 @@ render ctx (Line x1 y1 x2 y2) = do
   moveTo ctx x1 y1
   lineTo ctx x2 y2
   stroke ctx
+
 render ctx (Rect w h) = do
   rect ctx (-w/2) (-h/2) w h
   stroke ctx
+
+render ctx (FRect w' h') = do
+  -- rect ctx (-w/2) (-h/2) w h
+  fillRect ctx (-w/2) (-h/2) w h
+  stroke ctx
+  where
+    w = realToFrac w'
+    h = realToFrac h'
 
 render ctx (Combine c1 c2) = do
   render ctx c1
@@ -45,8 +60,27 @@ render ctx (Rotate angle' c) = do
   rotate ctx (-angle)
   where angle = realToFrac angle'
 
--- render ctx (SetFillColor color) = do
---   set
+render ctx (FillColor r g b a c) = do
+  let str = "rgba("
+         ++ intercalate "," [show r, show g, show b, show a]
+         ++ ")"
+  let color = toJSString str
+  setFillStyle ctx $ CanvasStyle color
+
+  render ctx c
+
+  setFillStyle ctx $ CanvasStyle $ toJSString "#000000"
+
+render ctx (StrokeColor r g b a c) = do
+  let str = "rgba("
+         ++ intercalate "," [show r, show g, show b, show a]
+         ++ ")"
+  let color = toJSString str
+  setStrokeStyle ctx $ CanvasStyle color
+
+  render ctx c
+
+  setStrokeStyle ctx $ CanvasStyle $ toJSString "#000000"
 
 render _ (Path []) = return ()
 render ctx (Path ((x, y) : points)) = do
@@ -73,6 +107,20 @@ render ctx (Polygon ((x, y) : points)) = do
           lineTo ctx px py
           renderPath ps
 
+render _ (FPolygon []) = return ()
+render ctx (FPolygon ((x, y) : points)) = do
+  moveTo ctx x y
+  renderPath points
+  closePath ctx
+  fill ctx $ Just CanvasWindingRuleNonzero
+  stroke ctx
+
+  -- TODO: can almost certainly do this neatly with mapM
+  where renderPath [] = return ()
+        renderPath ((px, py) : ps) = do
+          lineTo ctx px py
+          renderPath ps
+
 -- NOTE: do i need to move back after rendering a line
 render ctx (RegularPolygon sides radius) = do
   let step = (pi * 2) / (fromIntegral sides)
@@ -89,8 +137,27 @@ render ctx (RegularPolygon sides radius) = do
             poly (n + 1) step
       | otherwise = closePath ctx
 
+render ctx (FRegularPolygon sides radius) = do
+  let step = (pi * 2) / (fromIntegral sides)
+  moveTo ctx radius 0
+  poly 0 step
+  fill ctx $ Just CanvasWindingRuleNonzero
+  stroke ctx
+
+  where
+    poly n step
+      | n < (sides) = do
+            let x' = radius * (cos (step * (fromIntegral n)))
+            let y' = radius * (sin (step * (fromIntegral n)))
+            lineTo ctx x' y'
+            poly (n + 1) step
+      | otherwise = closePath ctx
+
 render ctx (Circle radius) = do
   render ctx (RegularPolygon 60 radius)
+
+render ctx (FCircle radius) = do
+  render ctx (FRegularPolygon 60 radius)
 
 render ctx (Text font width' text) = do
   setFont ctx font
